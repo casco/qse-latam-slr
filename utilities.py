f55327fefe5b27e8ea21e557997e02f1294cda4e
Alejandro Fernandez <alejandro.casco.fernandez@gmail.com>
2023-11-30 10:25:47 -0300

    add abstracts

--- utilities.py
+++ utilities.py
@@ -37,18 +37,18 @@ LATAM_COUNTRIES = ['Belize',
 def df_from_ieee_csv_file(file):
     ieee_df = pd.read_csv(file)
     ieee_df = ieee_df.filter(
-        items=['Document Title', 'Authors', 'Author Affiliations', 'Publication Title', 'Publication Year'], axis=1)
+        items=['Document Title', 'Authors', 'Author Affiliations', 'Publication Title', 'Publication Year', 'Abstract'], axis=1)
     ieee_df.rename(inplace=True,
                    columns={'Document Title': 'title', 'Authors': 'authors', 'Author Affiliations': 'affiliations',
-                            'Publication Title': 'publication', 'Publication Year': 'year'})
+                            'Publication Title': 'publication', 'Publication Year': 'year', 'Abstract': 'abstract'})
     return ieee_df
 
 
 def df_from_scopus_csv_file(file):
     scopus_df = pd.read_csv(file)
-    scopus_df = scopus_df.filter(items=['Title', 'Authors', 'Affiliations', 'Source Title', 'Year'], axis=1)
+    scopus_df = scopus_df.filter(items=['Title', 'Authors', 'Affiliations', 'Source Title', 'Year', 'Abstract'], axis=1)
     scopus_df.rename(inplace=True, columns={'Title': 'title', 'Authors': 'authors', 'Affiliations': 'affiliations',
-                                            'Source Title': 'publication', 'Year': 'year'})
+                                            'Source Title': 'publication', 'Year': 'year', 'Abstract': 'abstract'})
     return scopus_df
 
 
